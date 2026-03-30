3bf73b2cf5d1ba0dfa156ca6752bec760cf1d168
Prashant Sharma <prashant100ji@gmail.com>
2026-03-30 18:28:54 +0530

    fix category serilaizer

--- app/blueprints/category_blueprint.rb
+++ app/blueprints/category_blueprint.rb
@@ -6,6 +6,4 @@ class CategoryBlueprint < Blueprinter::Base
   field :business_profiles_count do |category|
     category.business_profiles.approved.count
   end
-
-  association :business_profiles, blueprint: BusinessProfileBlueprint
 end
